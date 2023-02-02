f8b5b684fbc9e63e2d3f0853d77ac7162e9313f3
Traore Adama Emmanuel <adama.emmanuel.traore.11@gmail.com>
2023-02-02 01:43:48 +0100

    Front-End Release 2.0

--- e-commerce-project/02-Back-End/spring-boot-ecommerce/src/main/java/com/shop/ecommerce/dao/ProductCategoryRepository.java
+++ e-commerce-project/02-Back-End/spring-boot-ecommerce/src/main/java/com/shop/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.shop.ecommerce.dao;
 import com.shop.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") // CONSENTE L'ACCESSO A QUALSIASI SITO WEB PER EVITARE ERRORI(VISTO CHE SPRING BOOT USA LA PORTA 8080, MENTRE ANGULAR USA 4200)
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category") // QUI HO MODIFICATO IL PERCORSO
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- e-commerce-project/02-Back-End/spring-boot-ecommerce/src/main/java/com/shop/ecommerce/dao/ProductRepository.java
+++ e-commerce-project/02-Back-End/spring-boot-ecommerce/src/main/java/com/shop/ecommerce/dao/ProductRepository.java
@@ -4,6 +4,6 @@ import com.shop.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:4200") // CONSENTE L'ACCESSO A QUALSIASI SITO WEB PER EVITARE ERRORI
+@CrossOrigin("http://localhost:4200") // CONSENTE L'ACCESSO A QUALSIASI SITO WEB PER EVITARE ERRORI(VISTO CHE SPRING BOOT USA LA PORTA 8080, MENTRE ANGULAR USA 4200)
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
